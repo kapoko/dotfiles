090b624cf031948bf13f6d8039dba7cb1047d8c7
kapoko <17455539+kapoko@users.noreply.github.com>
2021-11-08 23:27:24 +0100

    Added brewfile

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dotfiles
+# dotfiles 🌱
 
 
 ## Usage 
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,14 @@ info 'Installing symlinks'
 ln -svi ~/.dotfiles/.zshrc ~/.zshrc
 ln -svi ~/.dotfiles/.gitconfig ~/.gitconfig
 
-info 'Installing Homebrew'
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 info 'Installing Oh My ZSH'
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+info 'Installing Homebrew'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+info 'Installing brew formulae'
+brew bundle --file ~/.dotfiles/Brewfile
 
 source $ZSH/oh-my-zsh.sh
 
