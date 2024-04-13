e89b19efe1f3fd2520866656a886dbbb63535a5b
kapoko <17455539+kapoko@users.noreply.github.com>
2024-04-13 17:41:24 +0200

    Add treesitter

--- nvim/lua/kapoko/init.lua
+++ nvim/lua/kapoko/init.lua
@@ -1,2 +1,3 @@
+require("kapoko.set")
 require("kapoko.remap")
 require("kapoko.lazy_init")
---
+++ nvim/lua/kapoko/set.lua
@@ -0,0 +1,6 @@
+vim.opt.nu = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
