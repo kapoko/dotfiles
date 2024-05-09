5e594ca18284893ca931fe405664b07204285e13
kapoko <17455539+kapoko@users.noreply.github.com>
2024-05-09 10:36:48 +0200

    Set keymap for inline error viewing

--- nvim/lua/kapoko/remap.lua
+++ nvim/lua/kapoko/remap.lua
@@ -6,4 +6,4 @@ vim.keymap.set("n", "<C-k>", ":wincmd k<CR>");
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>");
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>");
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>");
-
+vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>");
