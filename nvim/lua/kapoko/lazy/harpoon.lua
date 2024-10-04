8f1cc930862224ee6f1255d54628484b7d52e7d9
kapoko <17455539+kapoko@users.noreply.github.com>
2024-10-04 10:14:43 +0200

    Make ctrl shift bindings work inside tmux

--- nvim/lua/kapoko/lazy/harpoon.lua
+++ nvim/lua/kapoko/lazy/harpoon.lua
@@ -11,7 +11,7 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
     end
 }
