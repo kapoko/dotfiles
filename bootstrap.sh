a27aa38ffec4fd8fdee432ccd4f3267d99ef2013
kapoko <17455539+kapoko@users.noreply.github.com>
2024-12-27 18:34:33 +0100

    Add ghostty and config

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,7 @@ ln -svf ~/.dotfiles/.sdirs ~/.sdirs
 ln -svf ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -svf ~/.dotfiles/.config/nvim ~/.config
 ln -svf ~/.dotfiles/.config/alacritty ~/.config
+ln -svf ~/.dotfiles/.config/ghostty ~/.config
 
 source $ZSH/oh-my-zsh.sh
 
