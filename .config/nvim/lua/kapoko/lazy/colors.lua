a4774bd529fed2ed27b34227fc000c91162f609f
kapoko <17455539+kapoko@users.noreply.github.com>
2025-07-24 10:03:01 +0200

    Change background color in nvim

--- .config/nvim/lua/kapoko/lazy/colors.lua
+++ .config/nvim/lua/kapoko/lazy/colors.lua
@@ -2,6 +2,9 @@ return {
     "bluz71/vim-nightfly-colors",
     priority = 1000, -- Ensure it loads first
     config = function()
-        vim.cmd.colorscheme "nightfly"
+        require("nightfly").custom_colors({
+            bg = "#0d1721",
+        })
+        vim.cmd([[colorscheme nightfly]])
     end
 }
