51c5f6cfcf73a30058b4e25d0d60cbeb8ac77d0c
kapoko <17455539+kapoko@users.noreply.github.com>
2026-03-31 14:22:03 +0200

    Untrack lazy-lock, cause we're on using dotfiles in a submodule elsewhere, nvim-treesitter follow main branch

--- .config/nvim/lua/kapoko/lazy/treesitter.lua
+++ .config/nvim/lua/kapoko/lazy/treesitter.lua
@@ -1,10 +1,11 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter").setup({
                 ensure_installed = {
                     "c", "lua", "vim", "vimdoc", "query",
                     "javascript", "typescript", "bash", "php", "glsl",
